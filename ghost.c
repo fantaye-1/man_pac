08ef7455a2e60185afa62fe0e633d6b77bc7e229
Chase Colman <chase@colman.io>
2018-10-07 18:20:31 -0400

    Add header/license info to ghost and manpac

--- ghost.c
+++ ghost.c
@@ -1,3 +1,14 @@
+/*
+ * A program -- TODO write what we do here
+ *
+ * Licensed under GPLv3.
+ * Copyright (c) 2018 Abrham Fantaye, Chase Colman, Youngsoo Kang.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
