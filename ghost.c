05b2db9b8500f432e1691b8444649b529302b2ef
yashito <yashito@gmail.com>
2018-10-30 00:04:36 -0400

    Reimplemented ghost movement into simpler form.

--- ghost.c
+++ ghost.c
@@ -26,6 +26,11 @@ init_ghost(int order)
 
 	ghost->pid = getpid();
 	ghost->order = order;
+	ghost->moveDir = 3; // Initialize to move to right direction
+	// Ghosts will initially line up at the upper left screen
+	ghost->pos.x = 5 + order * (ghost->dims.w + 5);
+	ghost->pos.y = 5;
+	
 	sem_post(&ghost->mutex);
 
 	return 0;
@@ -39,6 +44,107 @@ term_handler(int signal)
 	sem_wait(&ghost->mutex);
 	ghost->pid = 0;
 	sem_post(&ghost->mutex);
+}
+
+// Determines next position of this ghost.
+coord_t
+next_position(ghost_t *ghost, dims_t *screen)
+{
+	coord_t new_pos = ghost->pos;
+
+	// Move one pixel toward predefined direction
+	switch (ghost->moveDir)
+	{
+		case 0: // Move up
+			new_pos.y++;
+			break;
+		case 1: // Move down
+			new_pos.y--;
+			break;
+		case 2: // Move left
+			new_pos.x--;
+			break;
+		case 3: // Move right
+			new_pos.x++;
+	}
+
+	// Hard coded paths. Ghosts will move clockwise on the
+	//	perimeter line specified in the condition values.
+	switch (ghost->order)
+	{
+		case 0: // Circles around top left
+			if (new_pos.x < screen->w / 8) // @ Left boundary: move to top
+			{
+				ghost->moveDir = 0;
+			}
+			else if (new_pos.x > screen->w * 7 / 8) // @ Right boundary: move to bottom
+			{
+				ghost->moveDir = 1;
+			}
+			else if (new_pos.y < screen->h / 3) // @ Bottom boundary: move to left
+			{
+				ghost->moveDir = 2;
+			}
+			else if (new_pos.y > screen->h / 8) // @ Top boundary: move to right
+			{
+				ghost->moveDir = 3;
+			}
+			break;
+		case 1: // Circles around top right
+			if (new_pos.x < screen->w * 2 / 3)
+			{
+				ghost->moveDir = 0;
+			}
+			else if (new_pos.x > screen->w * 7 / 8)
+			{
+				ghost->moveDir = 1;
+			}
+			else if (new_pos.y < screen->h / 3)
+			{
+				ghost->moveDir = 2;
+			}
+			else if (new_pos.y > screen->h / 8)
+			{
+				ghost->moveDir = 3;
+			}
+			break;
+		case 2: // Circles around low middle
+			if (new_pos.x < screen->w / 3)
+			{
+				ghost->moveDir = 0;
+			}
+			else if (new_pos.x > screen->w * 2 / 3)
+			{
+				ghost->moveDir = 1;
+			}
+			else if (new_pos.y < screen->h / 8)
+			{
+				ghost->moveDir = 2;
+			}
+			else if (new_pos.y > screen->h * 2 / 3)
+			{
+				ghost->moveDir = 3;
+			}
+			break;
+		case 3:	// Circles around the entire screen
+			if (new_pos.x < screen->w / 4)
+			{
+				ghost->moveDir = 0;
+			}
+			else if (new_pos.x > screen->w * 3 / 4)
+			{
+				ghost->moveDir = 1;
+			}
+			else if (new_pos.y < screen->h / 4)
+			{
+				ghost->moveDir = 2;
+			}
+			else if (new_pos.y > screen->h * 3 / 4)
+			{
+				ghost->moveDir = 3;
+			}
+	}
+	return new_pos;
 }
 
 int
